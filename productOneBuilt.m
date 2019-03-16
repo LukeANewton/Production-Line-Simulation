a57c05d278cf025914ff545f2ec8b1103417ed74
Ryan Ribeiro <rmr@ryanribeiro.com>
2019-03-16 01:04:53 -0400

    productOneBuilt.m now generates next P1Event and C1Ready event

--- productOneBuilt.m
+++ productOneBuilt.m
@@ -1,20 +1,23 @@
 % Product 1
 function productOneBuilt()
-    global queueC1W1 inspectorOneBlocked;
+    global queueC1W1 inspectorOneBlocked W1Dist FEL clock P1Produced;
 
-    % Check if queue is empty
     if isQueueEmpty(queueC1W1)
        %TO DO: Set W1 to idle
-       return
-    % Not empty, so decrement queue size
     else
+        % Removes a C1 component from the queue
         queueC1W1 = queueC1W1 - 1;
-        % Is inspector 1 blocked
         if inspectorOneBlocked
             inspectorOneBlocked = false;
-            %TODO: generate C1Ready event AT CURRENT TIME, this will cause the inspector to
-            %try to place it's component again
+            % Generates C1Ready event AT CURRENT TIME
+            % This causes the inspector to try to place it's component again
+            e1 = Event(clock, EventType.C1Ready);
+            FEL.addEvent(e1);
         end
-         %TO DO: Generate next P1Build Event
+         % Generate next P1Build Event and add it to FEL
+         timeToAssemble = random(W1Dist);
+         e2 = Event(clock + timeToAssemble, EventType.P1Built);
+         FEL.addEvent(e2);
+         P1Produced = P1Produced + 1;
     end  
 end
