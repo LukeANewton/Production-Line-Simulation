5f868ca5ff7dd7bf821bde9ff5c9134c91ff138b
Luke Newton <28909353+LukeANewton@users.noreply.github.com>
2019-04-04 11:26:10 -0400

    trying to collect data from all designs
    work in progress - do not merge yet

--- manySim.m
+++ manySim.m
@@ -24,19 +24,24 @@ function manySim()
     global seed;
     %the length of one simulation run (set in Sim.m)
     global maxSimulationTime;
+    %arrays used to store statistics about each replication
+    global I1IdleTimes I2IdleTimes W1IdleTimes W2IdleTimes W3IdleTimes
+    global P1Productions P2Productions P3Productions totalProductions
+    global C1Inspections C2Inspections C3Inspections
+    global avgC1W1Sizes avgC1W2Sizes avgC1W3Sizes avgC2W2Sizes avgC3W3Sizes
 
     %---------------------------------------------
     %               Contol Variables
     %---------------------------------------------
     global numberOfReplications compareAlternateDesigns;
-    global alternativeStrategy alternativePriority;
+    global alternativeStrategy alternativePriority calculateReplicationsRequired;
     %Set calculateReplicationsRequired to true to have the program determine
     %how many replications are needed to get confidence intervals for
     %statistics of a width within 20% of the estimated value. If set to true,
     %the following numberOfReplications variable is ignored. The program will
     %always run at least 10 replications before determining if additional
     %replications are needed to shrink the confidence interval size
-    calculateReplicationsRequired = true; 
+    calculateReplicationsRequired = false; 
     %set numberOfReplications to the number of desired replications of the
     %simulation
     numberOfReplications = 5; 
@@ -56,15 +61,12 @@ function manySim()
     %for each alternative design, and then produce an output file containing
     %the comparison results. set to false to run multiple replications for one
     %system design which is spcified by the above two values.
-    compareAlternateDesigns = false;
+    compareAlternateDesigns = true;
     %---------------------------------------------
     %            End of Contol Variables
     %---------------------------------------------
 
     %start of main script
-    initializeRandomNumberStreams(seed);
-    initializeDistributions();
-
     %create directory for output files
     if ~exist('output', 'dir')
         mkdir('output');
@@ -73,91 +75,173 @@ function manySim()
     %delete old output files
     oldOutput = dir('output');
     for i = 1:size(oldOutput)
-        if(regexp(oldOutput(i).name, 'replication\d+.txt'))
+        if(regexp(oldOutput(i).name, 'Replication\d+.txt'))
              delete(strcat('output/', oldOutput(i).name));
+        elseif(regexp(oldOutput(i).name, 'FinalResults.txt'))
+            delete(strcat('output/', oldOutput(i).name));
         end
     end
-
+     
     if ~calculateReplicationsRequired
         %can only initialize these if we know for sure how many replications
-        I1IdleTimes = zeros(1, numberOfReplications);
-        I2IdleTimes = zeros(1, numberOfReplications);
-        W1IdleTimes = zeros(1, numberOfReplications);
-        W2IdleTimes = zeros(1, numberOfReplications);
-        W3IdleTimes = zeros(1, numberOfReplications);
-        P1Productions = zeros(1, numberOfReplications);
-        P2Productions = zeros(1, numberOfReplications);
-        P3Productions = zeros(1, numberOfReplications);
-        totalProductions = zeros(1, numberOfReplications);
-        C1Inspections = zeros(1, numberOfReplications);
-        C2Inspections = zeros(1, numberOfReplications);
-        C3Inspections = zeros(1, numberOfReplications);
-        avgC1W1Sizes = zeros(1, numberOfReplications);
-        avgC1W2Sizes = zeros(1, numberOfReplications);
-        avgC1W3Sizes = zeros(1, numberOfReplications);
-        avgC2W2Sizes = zeros(1, numberOfReplications);
-        avgC3W3Sizes = zeros(1, numberOfReplications);
+        if ~compareAlternateDesigns
+            arraySize = 1;
+        else
+            arraySize = 3;
+        end
+        I1IdleTimes = zeros(arraySize, numberOfReplications);
+        I2IdleTimes = zeros(arraySize, numberOfReplications);
+        W1IdleTimes = zeros(arraySize, numberOfReplications);
+        W2IdleTimes = zeros(arraySize, numberOfReplications);
+        W3IdleTimes = zeros(arraySize, numberOfReplications);
+        P1Productions = zeros(arraySize, numberOfReplications)
+        P2Productions = zeros(arraySize, numberOfReplications);
+        P3Productions = zeros(arraySize, numberOfReplications);
+        totalProductions = zeros(arraySize, numberOfReplications);
+        C1Inspections = zeros(arraySize, numberOfReplications);
+        C2Inspections = zeros(arraySize, numberOfReplications);
+        C3Inspections = zeros(arraySize, numberOfReplications);
+        avgC1W1Sizes = zeros(arraySize, numberOfReplications);
+        avgC1W2Sizes = zeros(arraySize, numberOfReplications);
+        avgC1W3Sizes = zeros(arraySize, numberOfReplications);
+        avgC2W2Sizes = zeros(arraySize, numberOfReplications);
+        avgC3W3Sizes = zeros(arraySize, numberOfReplications); 
     else
        %when calculating how many replications we need, always do at least 10
        numberOfReplications = 10; 
     end
+    
+    if ~compareAlternateDesigns
+         runManyReplications(); %run once with chosen design
+    else %run once with each design and compare values
+        alternativePriority = false;
+        alternativeStrategy = false; 
+        runManyReplications('DefaultDesign', 1);
+        alternativePriority = true;
+        alternativeStrategy = false;
+        runManyReplications('AltPriorityDesign', 2);
+        alternativePriority = false;
+        alternativeStrategy = true;
+        runManyReplications('AltStrategyDesign', 3);
+    end
+end
+
+function runManyReplications(filePrefix, statArrayIndex)
+    global compareAlternateDesigns
+    if ~compareAlternateDesigns
+        statArrayIndex = 1;
+    end
+
+    global numberOfReplications
+    %number values representing how long each inspector/workstation has spent idle
+    global Inspector1IdleTime Inspector2IdleTime;
+    global Workstation1IdleTime Workstation2IdleTime Workstation3IdleTime;
+    %integer values indicating the number of each product that has been produced
+    global P1Produced P2Produced P3Produced;
+    %integer values indicating the number of each component that has been inspected
+    global C1Inspected C2Inspected C3Inspected;
+    %arrays for storing the number of items in a queue each iteration
+    global arrayC1W1 arrayC1W2 arrayC2W2 arrayC1W3 arrayC3W3;
+    %random number generator seed
+    global seed;
+    %the length of one simulation run (set in Sim.m)
+    global maxSimulationTime;
+    %arrays used to store statistics about each replication
+    global I1IdleTimes I2IdleTimes W1IdleTimes W2IdleTimes W3IdleTimes
+    global P1Productions P2Productions P3Productions totalProductions
+    global C1Inspections C2Inspections C3Inspections
+    global avgC1W1Sizes avgC1W2Sizes avgC1W3Sizes avgC2W2Sizes avgC3W3Sizes
+    
+    initializeRandomNumberStreams(seed);
+    initializeDistributions();
 
     %run trials for specified number of times and collect statistics
     for i = 1:numberOfReplications
         %do one replication
-        outputFile = strcat('output/replication', num2str(i) , '.txt');
+        if ~compareAlternateDesigns
+            outputFile = strcat('output/Replication', num2str(i) , '.txt');
+        else
+            outputFile = strcat('output/',filePrefix,'Replication', num2str(i) , '.txt');
+        end
         Sim(false, outputFile);
         %collect statistics from single replication
-        I1IdleTimes(i) = Inspector1IdleTime;
-        I2IdleTimes(i) = Inspector2IdleTime;
-        W1IdleTimes(i) = Workstation1IdleTime;
-        W2IdleTimes(i) = Workstation2IdleTime;
-        W3IdleTimes(i) = Workstation3IdleTime;
-        P1Productions(i) = P1Produced;
-        P2Productions(i) = P2Produced;
-        P3Productions(i) = P3Produced;
-        totalProductions(i) = P1Produced + P2Produced + P3Produced;
-        C1Inspections(i) = C1Inspected;
-        C2Inspections(i) = C2Inspected;
-        C3Inspections(i) = C3Inspected;
-        avgC1W1Sizes(i) = mean(arrayC1W1);
-        avgC1W2Sizes(i) = mean(arrayC1W2);
-        avgC1W3Sizes(i) = mean(arrayC1W3);
-        avgC2W2Sizes(i) = mean(arrayC2W2);
-        avgC3W3Sizes(i) = mean(arrayC3W3);
+        I1IdleTimes(statArrayIndex,i) = Inspector1IdleTime;
+        I2IdleTimes(statArrayIndex,i) = Inspector2IdleTime;
+        W1IdleTimes(statArrayIndex,i) = Workstation1IdleTime;
+        W2IdleTimes(statArrayIndex,i) = Workstation2IdleTime;
+        W3IdleTimes(statArrayIndex,i) = Workstation3IdleTime;
+        P1Productions(statArrayIndex,i) = P1Produced
+        P2Productions(statArrayIndex,i) = P2Produced;
+        P3Productions(statArrayIndex,i) = P3Produced;
+        totalProductions(statArrayIndex,i) = P1Produced + P2Produced + P3Produced;
+        C1Inspections(statArrayIndex,i) = C1Inspected;
+        C2Inspections(statArrayIndex,i) = C2Inspected;
+        C3Inspections(statArrayIndex,i) = C3Inspected;
+        avgC1W1Sizes(statArrayIndex,i) = mean(arrayC1W1);
+        avgC1W2Sizes(statArrayIndex,i) = mean(arrayC1W2);
+        avgC1W3Sizes(statArrayIndex,i) = mean(arrayC1W3);
+        avgC2W2Sizes(statArrayIndex,i) = mean(arrayC2W2);
+        avgC3W3Sizes(statArrayIndex,i) = mean(arrayC3W3);
 
         %check if we need to do another - are CIs width < 20% of mean?
         if i >= 10 %we want to do at least 10 replications
-            if isCIWidthOver20Percent(P1Productions)||isCIWidthOver20Percent(P2Productions)||isCIWidthOver20Percent(P3Productions)||isCIWidthOver20Percent(C1Inspections)||isCIWidthOver20Percent(C2Inspections)||isCIWidthOver20Percent(C3Inspections)||isCIWidthOver20Percent(I1IdleTimes)||isCIWidthOver20Percent(I2IdleTimes)||isCIWidthOver20Percent(W1IdleTimes)||isCIWidthOver20Percent(W2IdleTimes)||isCIWidthOver20Percent(W3IdleTimes)||isCIWidthOver20Percent(avgC1W1Sizes)||isCIWidthOver20Percent(avgC1W2Sizes)||isCIWidthOver20Percent(avgC1W3Sizes)||isCIWidthOver20Percent(avgC2W2Sizes)||isCIWidthOver20Percent(avgC3W3Sizes)
-                %if any CI too large, do another replication
-                numberOfReplications = numberOfReplications + 1;
-            end
+            if ~compareAlternateDesigns
+                if isCIWidthOver20Percent(P1Productions)||isCIWidthOver20Percent(P2Productions)||isCIWidthOver20Percent(P3Productions)||isCIWidthOver20Percent(C1Inspections)||isCIWidthOver20Percent(C2Inspections)||isCIWidthOver20Percent(C3Inspections)||isCIWidthOver20Percent(I1IdleTimes)||isCIWidthOver20Percent(I2IdleTimes)||isCIWidthOver20Percent(W1IdleTimes)||isCIWidthOver20Percent(W2IdleTimes)||isCIWidthOver20Percent(W3IdleTimes)||isCIWidthOver20Percent(avgC1W1Sizes)||isCIWidthOver20Percent(avgC1W2Sizes)||isCIWidthOver20Percent(avgC1W3Sizes)||isCIWidthOver20Percent(avgC2W2Sizes)||isCIWidthOver20Percent(avgC3W3Sizes)
+                    %if any CI too large, do another replication
+                    numberOfReplications = numberOfReplications + 1;
+                end
+            else
+                if isCIWidthOver20Percent(P1Productions(statArrayIndex))||isCIWidthOver20Percent(P2Productions(statArrayIndex))||isCIWidthOver20Percent(P3Productions(statArrayIndex))||isCIWidthOver20Percent(C1Inspections(statArrayIndex))||isCIWidthOver20Percent(C2Inspections(statArrayIndex))||isCIWidthOver20Percent(C3Inspections(statArrayIndex))||isCIWidthOver20Percent(I1IdleTimes(statArrayIndex))||isCIWidthOver20Percent(I2IdleTimes(statArrayIndex))||isCIWidthOver20Percent(W1IdleTimes(statArrayIndex))||isCIWidthOver20Percent(W2IdleTimes(statArrayIndex))||isCIWidthOver20Percent(W3IdleTimes(statArrayIndex))||isCIWidthOver20Percent(avgC1W1Sizes(statArrayIndex))||isCIWidthOver20Percent(avgC1W2Sizes(statArrayIndex))||isCIWidthOver20Percent(avgC1W3Sizes(statArrayIndex))||isCIWidthOver20Percent(avgC2W2Sizes(statArrayIndex))||isCIWidthOver20Percent(avgC3W3Sizes(statArrayIndex))
+                    %if any CI too large, do another replication
+                    numberOfReplications = numberOfReplications + 1;
+                end
+            end          
         end
-
     end
 
     %print final statistics to file
     %average number of prodcts produced, components insepected, queue sizes
     %proportion of inspector and workstation times spent idle
     %variances and CIs for each value
-    fd = fopen('output/finalresults.txt', 'w');
-    printStatistic(fd, 'P1 Produced', P1Productions);
-    printStatistic(fd, 'P2 Produced', P2Productions);
-    printStatistic(fd, 'P3 Produced', P3Productions);
-    printStatistic(fd, 'Total Produced', totalProductions);
-    printStatistic(fd, 'C1 Inspected', C1Inspections);
-    printStatistic(fd, 'C2 Inspected', C2Inspections);
-    printStatistic(fd, 'C3 Inspected', C3Inspections);
-    printStatistic(fd, 'Workstation 1 C1 queue size', avgC1W1Sizes);
-    printStatistic(fd, 'Workstation 2 C1 queue size', avgC1W2Sizes);
-    printStatistic(fd, 'Workstation 2 C2 queue size', avgC2W2Sizes);
-    printStatistic(fd, 'Workstation 3 C1 queue size', avgC1W3Sizes);
-    printStatistic(fd, 'Workstation 3 C3 queue size', avgC3W3Sizes);
-    printStatistic(fd, 'Proportion of time Inspector 1 idle', I1IdleTimes/maxSimulationTime);
-    printStatistic(fd, 'Proportion of time Inspector 2 idle', I2IdleTimes/maxSimulationTime);
-    printStatistic(fd, 'Proportion of time Workstation 1 idle', W1IdleTimes/maxSimulationTime);
-    printStatistic(fd, 'Proportion of time Workstation 2 idle', W2IdleTimes/maxSimulationTime);
-    printStatistic(fd, 'Proportion of time Workstation 3 idle', W3IdleTimes/maxSimulationTime);
+    if ~compareAlternateDesigns
+        fd = fopen('output/FinalResults.txt', 'w');
+        printStatistic(fd, 'P1 Produced', P1Productions);
+        printStatistic(fd, 'P2 Produced', P2Productions);
+        printStatistic(fd, 'P3 Produced', P3Productions);
+        printStatistic(fd, 'Total Produced', totalProductions);
+        printStatistic(fd, 'C1 Inspected', C1Inspections);
+        printStatistic(fd, 'C2 Inspected', C2Inspections);
+        printStatistic(fd, 'C3 Inspected', C3Inspections);
+        printStatistic(fd, 'Workstation 1 C1 queue size', avgC1W1Sizes);
+        printStatistic(fd, 'Workstation 2 C1 queue size', avgC1W2Sizes);
+        printStatistic(fd, 'Workstation 2 C2 queue size', avgC2W2Sizes);
+        printStatistic(fd, 'Workstation 3 C1 queue size', avgC1W3Sizes);
+        printStatistic(fd, 'Workstation 3 C3 queue size', avgC3W3Sizes);
+        printStatistic(fd, 'Proportion of time Inspector 1 idle', I1IdleTimes/maxSimulationTime);
+        printStatistic(fd, 'Proportion of time Inspector 2 idle', I2IdleTimes/maxSimulationTime);
+        printStatistic(fd, 'Proportion of time Workstation 1 idle', W1IdleTimes/maxSimulationTime);
+        printStatistic(fd, 'Proportion of time Workstation 2 idle', W2IdleTimes/maxSimulationTime);
+        printStatistic(fd, 'Proportion of time Workstation 3 idle', W3IdleTimes/maxSimulationTime);
+    else
+        fd = fopen(strcat('output/',filePrefix,'FinalResults.txt'), 'w');
+        printStatistic(fd, 'P1 Produced', P1Productions(statArrayIndex));
+        printStatistic(fd, 'P2 Produced', P2Productions(statArrayIndex));
+        printStatistic(fd, 'P3 Produced', P3Productions(statArrayIndex));
+        printStatistic(fd, 'Total Produced', totalProductions(statArrayIndex));
+        printStatistic(fd, 'C1 Inspected', C1Inspections(statArrayIndex));
+        printStatistic(fd, 'C2 Inspected', C2Inspections(statArrayIndex));
+        printStatistic(fd, 'C3 Inspected', C3Inspections(statArrayIndex));
+        printStatistic(fd, 'Workstation 1 C1 queue size', avgC1W1Sizes(statArrayIndex));
+        printStatistic(fd, 'Workstation 2 C1 queue size', avgC1W2Sizes(statArrayIndex));
+        printStatistic(fd, 'Workstation 2 C2 queue size', avgC2W2Sizes(statArrayIndex));
+        printStatistic(fd, 'Workstation 3 C1 queue size', avgC1W3Sizes(statArrayIndex));
+        printStatistic(fd, 'Workstation 3 C3 queue size', avgC3W3Sizes(statArrayIndex));
+        printStatistic(fd, 'Proportion of time Inspector 1 idle', I1IdleTimes(statArrayIndex)/maxSimulationTime);
+        printStatistic(fd, 'Proportion of time Inspector 2 idle', I2IdleTimes(statArrayIndex)/maxSimulationTime);
+        printStatistic(fd, 'Proportion of time Workstation 1 idle', W1IdleTimes(statArrayIndex)/maxSimulationTime);
+        printStatistic(fd, 'Proportion of time Workstation 2 idle', W2IdleTimes(statArrayIndex)/maxSimulationTime);
+        printStatistic(fd, 'Proportion of time Workstation 3 idle', W3IdleTimes(statArrayIndex)/maxSimulationTime);
+    end
     fclose(fd);
 end
 
